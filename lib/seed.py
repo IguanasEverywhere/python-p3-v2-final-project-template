f45d3062b4150f203d930b8ed330100db5436de1
Scott Schwab <scottschwab86@gmail.com>
2023-09-12 15:14:37 -0500

    Update seed to persist several student instances to db

--- lib/seed.py
+++ lib/seed.py
@@ -1,5 +1,13 @@
 from models.collaborative_pianist import Collaborative_Pianist
+from models.student import Student
 
 def seed_table():
     Collaborative_Pianist.create("Scott", "Faculty", "scott@scott.com")
     Collaborative_Pianist.create("Maria", "TA", "maria@maria.com")
+
+    Student.create("John", "Sophomore", "Violin", 1)
+    Student.create("Anna", "Junior", "Flute")
+    Student.create("Bob", "Freshman", "Flute", 2)
+    Student.create("Michael", "Senior", "Trumpet", 2)
+
+
