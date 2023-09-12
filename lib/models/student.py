bc64a9afb9a1db2830dd8658bcccdcd289cef895
Scott Schwab <scottschwab86@gmail.com>
2023-09-12 14:37:04 -0500

    Add create and save_to_db methods to student class

--- lib/models/student.py
+++ lib/models/student.py
@@ -1,10 +1,11 @@
 from models.__init__ import CONN, CURSOR
+from models.collaborative_pianist import Collaborative_Pianist
 
 class Student:
 
     all_students = {}
 
-    def __init__(self, name, year, instrument, pianist_id = None, id=None):
+    def __init__(self, name, year, instrument, pianist_id, id=None):
         self.id = id
         self.name = name
         self.year = year
@@ -12,7 +13,7 @@ class Student:
         self.pianist_id = pianist_id
 
     def __repr__(self):
-        return f"{self.name}, {self.year}, {self.instrument}"
+        return f"{self.name}, {self.year}, {self.instrument}, {self.pianist_id}"
 
     @classmethod
     def create_table(cls):
@@ -37,3 +38,20 @@ class Student:
 
         CURSOR.execute(sql)
         CONN.commit()
+
+    @classmethod
+    def create(cls, name, year, instrument, pianist_id=1):
+        new_student = cls(name, year, instrument, pianist_id)
+        print(new_student)
+        new_student.save_to_db()
+        return new_student
+
+    def save_to_db(self):
+        sql = """
+            INSERT INTO students (name, year, instrument, pianist_id)
+            VALUES (?, ?, ?, ?)
+            """
+        print(self)
+        CURSOR.execute(sql, (self.name, self.year, self.instrument, self.pianist_id))
+        CONN.commit()
+        print(f"{self.name} saved to the database!")
