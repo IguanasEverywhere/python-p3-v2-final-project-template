b042c3b44874ab313c0e61dbc6eba4d24b373e62
Scott Schwab <scottschwab86@gmail.com>
2023-09-18 09:53:17 -0500

    Add a non pianist for unassigned students to db

--- lib/seed.py
+++ lib/seed.py
@@ -2,12 +2,13 @@ from models.collaborative_pianist import Collaborative_Pianist
 from models.student import Student
 
 def seed_table():
+    Collaborative_Pianist.create("No Pianist", "Faculty", "None@none.com")
     Collaborative_Pianist.create("Scott", "Faculty", "scott@scott.com")
     Collaborative_Pianist.create("Maria", "TA", "maria@maria.com")
 
-    Student.create("John", "Sophomore", "Violin", 1)
-    Student.create("Anna", "Junior", "Flute", 2)
-    Student.create("Bob", "Freshman", "Flute", 2)
-    Student.create("Michael", "Senior", "Trumpet", 2)
+    Student.create("John", "Sophomore", "Violin", 2)
+    Student.create("Anna", "Junior", "Flute", 1)
+    Student.create("Bob", "Freshman", "Flute", 3)
+    Student.create("Michael", "Senior", "Trumpet", 3)
 
 
